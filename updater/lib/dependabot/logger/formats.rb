e617196f47e2e095460b2d01c6333270380b3a24
Jamie Magee <jamagee@microsoft.com>
2023-11-02 16:01:34 -0700

    Add types for logger formats

--- updater/lib/dependabot/logger/formats.rb
+++ updater/lib/dependabot/logger/formats.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "logger"
+require "sorbet-runtime"
 
 # Provides Logger::Formatter classes specific to the Updater project to augment
 # the global log helper defined in common/lib/dependabot/logger.rb
@@ -10,19 +11,42 @@ module Dependabot
     TIME_FORMAT = "%Y/%m/%d %H:%M:%S"
 
     class BasicFormatter < ::Logger::Formatter
+      extend T::Sig
+
+      sig do
+        params(
+          severity: Integer,
+          _datetime: Time,
+          _progname: String,
+          msg: String
+        )
+          .returns(String)
+      end
       def call(severity, _datetime, _progname, msg)
         "#{Time.now.strftime(TIME_FORMAT)} #{severity} #{msg2str(msg)}\n"
       end
     end
 
     class JobFormatter < ::Logger::Formatter
+      extend T::Sig
+
       CLI_ID = "cli"
       UNKNOWN_ID = "unknown_id"
 
+      sig { params(job_id: T.nilable(String)).void }
       def initialize(job_id)
         @job_id = job_id
       end
 
+      sig do
+        params(
+          severity: Integer,
+          _datetime: Time,
+          _progname: String,
+          msg: String
+        )
+          .returns(String)
+      end
       def call(severity, _datetime, _progname, msg)
         [
           Time.now.strftime(TIME_FORMAT),
@@ -34,6 +58,7 @@ module Dependabot
 
       private
 
+      sig { returns(T.nilable(String)) }
       def job_prefix
         return @job_prefix if defined? @job_prefix
         # The dependabot/cli tool uses a placeholder value since it does not
